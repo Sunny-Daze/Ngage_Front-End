3803f380d00305447a39def9e098f70f51d84381
Sunny-Daze7 <sunnyadav0727@gmail.com>
2022-10-30 21:08:07 +0530

    login page & register page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,44 +1,130 @@
-import React, {useState} from 'react';
-import './Login.css';
-import { loginUser } from '../services/api';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Login.css";
+import { loginUser } from "../services/api";
+import { useNavigate } from "react-router-dom";
+import {
+  FormGroup,
+  TextField,
+  Typography,
+  FormControl,
+  Button,
+  styled,
+  Link,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Grid from "@mui/material/Grid";
+
+const Container = styled(FormGroup)`
+  margin-left: 35px;
+  & > div {
+    margin-top: 20px;
+  }
+  & > a {
+    margin-top: 15px;
+  }
+`;
 
 const defaulValue = {
-    email:'',
-    password:''
-}
+  email: "",
+  password: "",
+};
 
 function Login() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [userData, setUserData] = useState(defaulValue);
 
-    const [userData, setUserData] = useState(defaulValue);
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
 
-    const handleChange = (e) => {
-        setUserData({...userData, [e.target.name]:e.target.value});
-    }
-
-    const userLogin = async () => {
-        await loginUser(userData);
-    }
+  const userLogin = async () => {
+    await loginUser(userData);
+  };
 
   return (
     <div>
-        <div className="Login">
-        <div className='BannerPicture'></div>
-        <div className="form">
-        <h1>Login</h1>
-        <input type="text" placeholder='Email' name='email' onChange={(e)=>{handleChange(e)}} />
-        <br />
-        <input type="password" placeholder='Password' name='password' onChange={(e)=>{handleChange(e)}} />
-        <br />
-        <button onClick={userLogin}>Login</button>
-        <br />
-        <button onClick={() => navigate('/register')}>Register</button>
+      <div className="Login">
+        <div className="BannerPicture"></div>
+        <div className="Form">
+          <Container>
+            <Grid item xs={1}>
+              <LockOutlinedIcon
+                style={{
+                  color: "white",
+                  background: "#8B008B",
+                  borderRadius: "50%",
+                  padding: "5",
+                }}
+              />
+            </Grid>
+            <Typography
+              variant="h6"
+              style={{ fontSize: "18px", marginTop: "5px" }}
+            >
+              LOG IN
+            </Typography>
+            <Typography
+              variant="h6"
+              style={{
+                fontSize: "13px",
+                width: "25vw",
+                lineHeight: "1.1",
+                marginTop: "10px",
+              }}
+            >
+              By continuing, you agree to our{" "}
+              <Link href="#" onclick underline="always">
+                {"User Agreement"}
+              </Link>{" "}
+              and{" "}
+              <Link href="#" onclick underline="always">
+                {"Privacy Policy"}
+              </Link>
+              .
+            </Typography>
+            <FormControl style={{ width: "25vw" }}>
+              <TextField
+                required
+                id="outlined-required"
+                label="Email Address"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                name="email"
+              />
+            </FormControl>
+            <FormControl style={{ width: "25vw" }}>
+              <TextField
+                required
+                id="outlined-required"
+                label="Password"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                name="password"
+                type="password"
+              />
+            </FormControl>
+            <FormControl>
+              <Button
+                variant="contained"
+                style={{ width: "25vw" }}
+                onClick={userLogin}
+              >
+                LOG IN
+              </Button>
+            </FormControl>
+            <div onClick={() => navigate("/register")}>
+              <Link className='link' underline="always">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </div>
+          </Container>
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,54 +1,160 @@
-import React, { useState } from 'react';
-import './Register.css';
-import { userRegister } from '../services/api';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Register.css";
+import { userRegister } from "../services/api";
+import { useNavigate } from "react-router-dom";
+import {
+  FormGroup,
+  TextField,
+  Typography,
+  FormControl,
+  Button,
+  styled,
+  Link,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Grid from "@mui/material/Grid";
+
+const Container = styled(FormGroup)`
+  margin-left: 35px;
+  & > div {
+    margin-top: 20px;
+  }
+  & > a {
+    margin-top: 15px;
+  }
+`;
 
 const defaulValue = {
-    name:'',
-    email:'',
-    password:'',
-    reEnterPassword:''
-}
+  name: "",
+  email: "",
+  password: "",
+  reEnterPassword: "",
+};
 
 function Register() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [userData, setUserData] = useState(defaulValue);
+  const [userData, setUserData] = useState(defaulValue);
 
-    const handleChange = (e) => {
-        setUserData({...userData, [e.target.name]:e.target.value});
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const userRegisteration = async () => {
+    const { name, email, password, reEnterPassword } = userData;
+    console.log(name, email, password, reEnterPassword);
+    if (name && email && password && password === reEnterPassword) {
+      await userRegister(userData);
+    } else {
+      alert("Invalid input");
     }
-
-    const userRegisteration = async () => {
-        const { name, email, password, reEnterPassword} = userData;
-        console.log(name, email, password, reEnterPassword)
-        if(name && email && password && (password === reEnterPassword)){
-            await userRegister(userData);
-        }
-        else {
-            alert("Invalid input");
-        }
-    }
+  };
 
   return (
-    <div className='Register'>
-        <div className='BannerPicture'></div>
-        <div className='Form'>
-        <h1>Register</h1>
-        <input type="text" placeholder='Name' onChange={(e)=>{handleChange(e)}} name='name'/>
-        <br />
-        <input type="email" placeholder='Email' onChange={(e)=>{handleChange(e)}} name='email'/>
-        <br />
-        <input type="password" placeholder='Password' onChange={(e)=>{handleChange(e)}} name='password'/>
-        <br />
-        <input type="password" placeholder='Re-enter Password' onChange={(e)=>{handleChange(e)}} name='reEnterPassword'/>
-        <br />
-        <button onClick={userRegisteration}>Register</button>
-        <br />
-        <button onClick={() => navigate('/')}>Login</button>
-        </div>
+    <div className="Register">
+      <div className="BannerPicture">
+      </div>
+      <div className="Form">
+        <Container>
+          <Grid item xs={1}>
+            <LockOutlinedIcon
+              style={{
+                color: "white",
+                background: "#8B008B",
+                borderRadius: "50%",
+                padding: "5",
+              }}
+            />
+          </Grid>
+          <Typography
+            variant="h6"
+            style={{ fontSize: "18px", marginTop: "5px" }}
+          >
+            SIGN UP
+          </Typography>
+          <Typography
+            variant="h6"
+            style={{
+              fontSize: "13px",
+              width: "25vw",
+              lineHeight: "1.1",
+              marginTop: "10px",
+            }}
+          >
+            By continuing, you are setting up a Ngage account and agree to our{" "}
+            <Link href="#" onclick underline="always">
+              {"User Agreement"}
+            </Link>{" "}
+            and{" "}
+            <Link href="#" onclick underline="always">
+              {"Privacy Policy"}
+            </Link>
+            .
+          </Typography>
+          <FormControl style={{ width: "25vw" }}>
+            <TextField
+              required
+              id="outlined-required"
+              label="Email Address"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="email"
+            />
+          </FormControl>
+          <FormControl style={{ width: "25vw" }}>
+            <TextField
+              required
+              id="outlined-required"
+              label="User Name"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="name"
+            />
+          </FormControl>
+          <FormControl style={{ width: "25vw" }}>
+            <TextField
+              required
+              id="outlined-required"
+              label="Password"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="password"
+              type="password"
+            />
+          </FormControl>
+          <FormControl style={{ width: "25vw" }}>
+            <TextField
+              required
+              id="outlined-required"
+              label="Confirm-Password"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="reEnterPassword"
+              type="password"
+            />
+          </FormControl>
+          <FormControl>
+            <Button
+              variant="contained"
+              style={{ width: "25vw" }}
+              onClick={userRegisteration}
+            >
+              SIGN UP
+            </Button>
+          </FormControl>
+          <div onClick={() => navigate("/")}>
+            <Link className='link' onclick underline="always">
+              {"Already have a account? Sign In"}
+            </Link>
+          </div>
+        </Container>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
