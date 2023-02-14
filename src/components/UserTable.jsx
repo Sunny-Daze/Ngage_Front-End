9170779ad1d60e8aa17a82ce1e25e7d6f7eaac62
Sunny-Daze7 <sunnyadav0727@gmail.com>
2023-02-15 02:01:47 +0530

    user-control

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,51 +1,153 @@
-import React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Button, IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import "./UserTable.css";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: "email", headerName: "Email", width: 200 },
+  { field: "username", headerName: "User Name", width: 200 },
+  { field: "role", headerName: "Role", width: 180 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    field: "editbutton",
+    headerName: "",
+    sortable: false,
+    headerClassName: "lastcolumnSeparator",
+    width: 100,
+    renderCell: (params) => {
+      const onClick = (e) => {
+        e.stopPropagation(); // don't select this row after clicking
+
+        const api: GridApi = params.api;
+        const thisRow: Record<string, GridCellValue> = {};
+
+        api
+          .getAllColumns()
+          .filter((c) => c.field !== "__check__" && !!c)
+          .forEach(
+            (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+          );
+          window.location.href = '/store';
+        // return alert(JSON.stringify(thisRow, null, 4));
+      };
+
+      return (
+        <Button
+          variant="outlined"
+          size="small"
+          style={{ marginLeft: 25, borderColor: "#001f54", color: "#001f54" }}
+          onClick={onClick}
+        >
+          <EditIcon />
+        </Button>
+      );
+    },
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
+    field: "deletebutton",
+    headerName: "",
+    hidable: true,
     sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    headerClassName: "lastcolumnSeparator",
+    width: 100,
+    renderCell: (params) => {
+      const onClick = (e) => {
+        e.stopPropagation(); // don't select this row after clicking
+
+        const api: GridApi = params.api;
+        const thisRow: Record<string, GridCellValue> = {};
+
+        api
+          .getAllColumns()
+          .filter((c) => c.field !== "__check__" && !!c)
+          .forEach(
+            (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+          );
+
+        return alert(JSON.stringify(thisRow, null, 4));
+      };
+
+      return (
+        <IconButton style={{ color: "red" }}>
+          <DeleteIcon />
+        </IconButton>
+      );
+    },
   },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    id: 1,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
+  {
+    id: 2,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
+  {
+    id: 3,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
+  {
+    id: 4,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
+  {
+    id: 5,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
+  {
+    id: 6,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
+  {
+    id: 7,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
+  {
+    id: 8,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
+  {
+    id: 9,
+    email: "employee@gmail.com",
+    username: "employee",
+    role: "employee",
+  },
 ];
 
 function UserTable() {
-    return(
-        <div style={{ height: 400, width: '100%' }}>
+  const [openModal, setOpenModal] = React.useState(false);
+  return (
+    <div style={{ height: 400, width: "100%" }}>
       <DataGrid
+        rowHeight={40}
+        style={{ fontSize: "0.8rem" }}
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        // checkboxSelection
+        pageSize={6}
+        rowsPerPageOptions={[6]}
       />
     </div>
-    )
+  );
 }
 
 export default UserTable;
--- src/pages/UserControl.jsx
+++ src/pages/UserControl.jsx
@@ -2,20 +2,8 @@ import React from "react";
 import Card from "../widgets/HeadingBanner";
 import UserTable from "../components/UserTable";
 import "./UserControl.css";
-import {
-  styled,
-  alpha,
-  InputBase,
-  Button,
-  Typography,
-  Box,
-  Modal,
-  TextField,
-  MenuItem,
-} from "@mui/material";
+import { styled, alpha, InputBase } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { domain, endPoints } from "../services/endPoints";
-import {postMethod} from "../services/Http.services";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -51,55 +39,14 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
     width: "50%",
-    height: "20%",
+    height:"20%",
     [theme.breakpoints.up("md")]: {
       width: "20ch",
     },
   },
 }));
 
-// Add New User modal
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 600,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
-// User role Options
-
-let userRoles = ["ADMIN", "EMPLOYEE", "USER", "COOPERATE"];
-
-async function saveNewUser(fullName, email, role, phoneNumber, id) {
-  let respone = await postMethod(domain + endPoints.createuser, {
-    email: email,
-    password: "ngage123",
-    fullName: fullName,
-    phone: phoneNumber,
-    countryCode: "+91",
-    role: role,
-  });
-
-
-  console.log(respone);
-}
-
 function UserControl() {
-  const [fullName, setfullName] = React.useState("");
-  const [email, setemail] = React.useState("");
-  const [phoneNumber, setphoneNumber] = React.useState("");
-
-  const [role, setrole] = React.useState("");
-
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
   return (
     <div className="User-Control">
       <Card
@@ -113,98 +60,10 @@ function UserControl() {
               <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
-              placeholder="Search…"
+              placeholder="Search for user"
               inputProps={{ "aria-label": "search" }}
             />
           </Search>
-
-          <Button onClick={handleOpen} variant="contained">
-            Contained
-          </Button>
-
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Create New User
-              </Typography>
-
-              <div className="add-user-form ">
-                <div className="mb-2 w-5">
-                  <TextField
-                    required
-                    id="outlined-required"
-                    label="Full Name"
-                    defaultValue=""
-                    value={fullName}
-                    onChange={(value) => setfullName(value.target.value)}
-                  />
-                </div>
-
-                <div className="mb-2 w-5">
-                  <TextField
-                    required
-                    id="outlined-required"
-                    label="Email "
-                    defaultValue=""
-                    value={email}
-                    onChange={(value) => setemail(value.target.value)}
-                  />
-                </div>
-
-                <div className="mb-2 w-5">
-                  <TextField
-                    required
-                    id="outlined-required"
-                    label="Phone Number"
-                    defaultValue=""
-                    value={phoneNumber}
-                    onChange={(value) => setphoneNumber(value.target.value)}
-                  />
-                </div>
-
-                <div className="mb-2 w-5">
-                  <TextField
-                    id="user-role"
-                    select
-                    size="15"
-                    label="Role"
-                    defaultValue=""
-                    helperText="Please select user's role"
-                    value={role}
-                    onChange={(value) => setrole(value.target.value)}
-                  >
-                    {userRoles.map((option) => (
-                      <MenuItem key={option} value={option}>
-                        {option}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                </div>
-              </div>
-
-              <div className="bottom-buttons">
-                <div className="mr-2">
-                  <Button onClick={handleClose} variant="outlined">
-                    Cancel
-                  </Button>
-                </div>
-
-                <div className="">
-                  <Button
-                    onClick={() => saveNewUser(fullName, email, role, "")}
-                    variant="contained"
-                  >
-                    Save User
-                  </Button>
-                </div>
-              </div>
-            </Box>
-          </Modal>
         </div>
         <UserTable />
       </div>
