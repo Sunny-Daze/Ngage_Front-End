5a15ae235849afa4382b06fd742cb3e9f56a0ce6
Sunny-Daze7 <sunnyadav0727@gmail.com>
2023-02-18 15:34:45 +0530

    learning management

---
+++ src/pages/LearningManagement.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./LearningManagement.css";
+import Card from "../widgets/CardWithPoints";
+import { Divider, styled } from "@mui/material";
+import CourseCard from '../widgets/ActiveCourseCard';
+
+const Root = styled('div')(({ theme }) => ({
+    width: '100%',
+    ...theme.typography.body2,
+    '& > :not(style) + :not(style)': {
+      marginTop: theme.spacing(2),
+    },
+  }));
+
+function LearningManagement() {
+  return (
+    <div className="LearningManagement">
+      <Card
+        heading="Learning Management"
+        subheading="lorem ipsum, dolor sit amet consectetur adipisicing elit"
+        coins="100"
+      />
+      <Root>
+      <Divider style={{marginTop:'0.8rem', marginBottom:'1rem'}}>Your Traingings</Divider>
+      </Root>
+
+        <div className="TrainingCourses">
+      <CourseCard />
+        </div>
+
+    </div>
+  );
+}
+
+export default LearningManagement;
