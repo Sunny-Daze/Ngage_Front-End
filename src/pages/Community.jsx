ab419333bbf126f56f6eb7d6c7e4d65e690f3181
Sunny-Daze7 <sunnyadav0727@gmail.com>
2023-02-23 00:42:48 +0530

    community

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Community.css";
-import { TextField, InputLabel, MenuItem, FormControl, Select, Button, Divider } from '@mui/material'
-import AddIcon from '@mui/icons-material/Add';
+import {
+  TextField,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Button,
+  Divider,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import Post from '../widgets/Post'
 // import PostComponent from "../widgets/post-components";
 
 function Community() {
-  const [filter, setFilter] = React.useState('none');
-  const [sort, setSort] = React.useState('newest');
+  const [filter, setFilter] = React.useState("none");
+  const [sort, setSort] = React.useState("newest");
 
   const handleFilter = (event) => {
     setFilter(event.target.value);
@@ -19,44 +28,73 @@ function Community() {
   return (
     <div className="Community">
       <div className="CommunitySearchBar">
-      <TextField style={{width:'23rem'}} size="small" label="Search for Posts" id="outlined-size-small" />
-      <FormControl style={{width:'9rem'}}>
-        <InputLabel id="demo-simple-select-label">Filter</InputLabel>
-        <Select
-          autoHeight
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={filter}
-          label="Age"
+        <TextField
+          style={{ width: "23rem" }}
           size="small"
-          onChange={handleFilter}
-        >
-          <MenuItem value={'none'}>none</MenuItem>
-          <MenuItem value={'question'}>Question</MenuItem>
-          <MenuItem value={'discussion'}>Discussion</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl style={{width:'9rem'}}>
-        <InputLabel size="small" id="demo-simple-select-label">Sort</InputLabel>
-        <Select
-          autoHeight
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={sort}
-          label="Age"
-          size="small"
-          onChange={handleSort}
-        >
-          <MenuItem value={'newest'}>Newest</MenuItem>
-          <MenuItem value={'oldest'}>Oldest</MenuItem>
-          <MenuItem value={'likes'}>Likes</MenuItem>
-        </Select>
-      </FormControl>
+          label="Search for Posts"
+          id="outlined-size-small"
+        />
+        <FormControl style={{ width: "9rem" }}>
+          <InputLabel id="demo-simple-select-label">Filter</InputLabel>
+          <Select
+            autoHeight
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={filter}
+            label="Age"
+            size="small"
+            onChange={handleFilter}
+          >
+            <MenuItem value={"none"}>none</MenuItem>
+            <MenuItem value={"question"}>Question</MenuItem>
+            <MenuItem value={"discussion"}>Discussion</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl style={{ width: "9rem" }}>
+          <InputLabel size="small" id="demo-simple-select-label">
+            Sort
+          </InputLabel>
+          <Select
+            autoHeight
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={sort}
+            label="Age"
+            size="small"
+            onChange={handleSort}
+          >
+            <MenuItem value={"newest"}>Newest</MenuItem>
+            <MenuItem value={"oldest"}>Oldest</MenuItem>
+            <MenuItem value={"likes"}>Likes</MenuItem>
+          </Select>
+        </FormControl>
       </div>
 
-      <Divider style={{marginTop:'1.4rem', marginBottom:'1rem', width:'50rem'}}/>
+      <Divider
+        style={{ marginTop: "1.4rem", marginBottom: "0.6rem", width: "50rem" }}
+      />
       <div className="createPostButton">
-      <Button variant='contained' size='small' style={{width:'9rem', fontSize:'0.75rem', background:'#001f54'}}>create post <AddIcon style={{marginLeft:'0.2rem', fontSize:'1.2rem'}}/></Button>
+        <Button
+          variant="contained"
+          style={{ width: "9rem", fontSize: "0.75rem", background: "#001f54" }}
+        >
+          create post{" "}
+          <AddIcon style={{ marginLeft: "0.2rem", fontSize: "1.2rem" }} />
+        </Button>
+      </div>
+
+      <div className="postBody">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
---
+++ src/widgets/Post.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Post.css";
+import { Button } from "@mui/material";
+// import couponImage from "../assets/couponImage.png";
+
+function Post(props) {
+  return (
+    <div className="PostCard">
+        <div className="postUtils">
+            <div className="postUtilsIcons">
+                
+            </div>
+        </div>
+        <div className="postCardBody">hwllo</div>
+    </div>
+  );
+}
+
+export default Post;
