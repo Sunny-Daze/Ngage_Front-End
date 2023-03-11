c1c52c64cf1b7f6ed96d707c81eab377de2ec353
Sunny-Daze7 <sunnyadav0727@gmail.com>
2023-03-12 04:39:08 +0530

    comments

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
-import './Comments.css'
+import React from "react";
+import "./Comments.css";
+import PostComment from '../widgets/PostComment';
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 function Comments() {
   return (
     <div className="Comments">
-        <div className="Comments-Post">
+      <div className="Comments-Post"></div>
+
+      <div className="Post-Comments">
+        <div className="Comments-Form">
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "92ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="standard-multiline-static"
+            //   label="Write your comment here..."
+              multiline
+              placeholder="Write your comment here..."
+              rows={3}
+              variant="standard"
+            />
+            <Button variant="contained" size="small" style={{background:'#001f54', marginLeft:'40.6rem', fontSize:'0.8rem'}} >comment</Button>
+          </Box>
+
+            <div className="Comments-list">
+              <PostComment />
+              <PostComment />
+              <PostComment />
+              <PostComment />
+              <PostComment />
+              <PostComment />
+              <PostComment />
+              <PostComment />
+              <PostComment />
+              <PostComment />
+              <PostComment />
+            </div>
 
         </div>
-
-        <div className="Post-Comments">
-            sdf
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Comments
+export default Comments;
