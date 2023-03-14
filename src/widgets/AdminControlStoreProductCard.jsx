1e1bf78f4ff0441dfe8410cbd7716483bcf78678
Sunny-Daze7 <sunnyadav0727@gmail.com>
2023-03-14 22:23:01 +0530

    delete modal

--- src/widgets/AdminControlStoreProductCard.jsx
+++ src/widgets/AdminControlStoreProductCard.jsx
@@ -1,26 +1,72 @@
-import React from 'react'
-import './AdminControlStoreProductCard.css'
-import {Typography,Button} from '@mui/material'
+import React from "react";
+import "./AdminControlStoreProductCard.css";
+import { Typography, Button } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import DeleteModal from '../components/DeleteModal';
 
 function AdminControlStoreProductCard() {
-  return (
-    <div className='AdminControlStoreProductCard'>
-        <div className="admin-control-store-left-container">
+  const [deleteModalSwitch, setDeleteModalSwitch] = React.useState(false);
 
-        </div>
-        <div className="admin-control-store-right-container">
-            <Typography variant='body2'>Product title: Food Coupon</Typography>
-            <Typography variant='body2'>Product body: Delicious large pizza</Typography>
-            <Typography variant='body2'>Product cost: 3000</Typography>
+  const handleProductDelete = () => {
+    setDeleteModalSwitch(true);
+
+  }
+
+  const deleteProduct = () => {
+    console.log("Post deleted successfully!");
+  }
+
+  return (
+    <div className="AdminControlStoreProductCard">
+      <DeleteModal deleteModalSwitch={deleteModalSwitch} deletefunction={deleteProduct} thing="Food Coupon" setDeleteModalSwitch={setDeleteModalSwitch} />
+      <div className="admin-control-store-left-container"></div>
+      <div className="admin-control-store-right-container">
+        <Typography variant="body2">Product title: Food Coupon</Typography>
+        <Typography variant="body2">
+          Product body: Delicious large pizza
+        </Typography>
+        <Typography variant="body2">Product cost: 3000</Typography>
         <div className="admin-control-store-products-buttons">
-            <Button size="small" variant="outlined" style={{marginRight:'1rem', color:'#001f54', borderColor:'#001f54'}}>edit item <EditIcon style={{color:'#001f54', fontSize:"1.15rem", marginLeft:'0.2rem', marginBottom:'0.2rem'}} /></Button>
-            <Button size="small" variant="outlined" style={{borderColor:'red', color:'red'}}>remove item <DeleteIcon style={{color:'red', fontSize:"1.15rem", marginLeft:'0.2rem', marginBottom:'0.2rem'}} /></Button>
+          <Button
+            size="small"
+            variant="outlined"
+            style={{
+              marginRight: "1rem",
+              color: "#001f54",
+              borderColor: "#001f54",
+            }}
+          >
+            edit item{" "}
+            <EditIcon
+              style={{
+                color: "#001f54",
+                fontSize: "1.15rem",
+                marginLeft: "0.2rem",
+                marginBottom: "0.2rem",
+              }}
+            />
+          </Button>
+          <Button
+          onClick={handleProductDelete}
+            size="small"
+            variant="outlined"
+            style={{ borderColor: "red", color: "red" }}
+          >
+            remove item{" "}
+            <DeleteIcon
+              style={{
+                color: "red",
+                fontSize: "1.15rem",
+                marginLeft: "0.2rem",
+                marginBottom: "0.2rem",
+              }}
+            />
+          </Button>
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AdminControlStoreProductCard
+export default AdminControlStoreProductCard;
--- src/widgets/CustomizedSnackbar.jsx
+++ src/widgets/CustomizedSnackbar.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
-
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
+// import MuiAlert from '@mui/material/Alert';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+// const Alert = React.forwardRef(function Alert(props, ref) {
+//   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+// });
 
 export default function CustomizedSnackbar(props) {
 
@@ -22,7 +23,9 @@ export default function CustomizedSnackbar(props) {
   return (
     <Stack spacing={2} sx={{ width: '100%' }}>
       <Snackbar open={props.snackbarSwitch} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{vertical, horizontal}}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+        {/* <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}> */}
+        <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
           {props.snackbarMessage}
         </Alert>
       </Snackbar>
