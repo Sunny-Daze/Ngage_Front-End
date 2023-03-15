baee31afb88d7e1ddff986c48cbac6b48f8456fb
Sunny-Daze7 <sunnyadav0727@gmail.com>
2023-03-15 19:44:45 +0530

    add product modal

--- src/pages/AdminControlStore.jsx
+++ src/pages/AdminControlStore.jsx
@@ -3,12 +3,15 @@ import './AdminControlStore.css'
 import AdminControlStoreProductCard from '../widgets/AdminControlStoreProductCard'
 import {Button} from '@mui/material'
 import AddIcon from "@mui/icons-material/Add";
-
+import AddProductModal from '../components/AddProductModal'
 function AdminControlStore() {
+  const [createModal, setCreateModal] = React.useState(false)
+
   return (
     <div className='AdminControlStore'>
+      <AddProductModal open={createModal} close={setCreateModal} />
       <div className="AdminControlButton">
-        <Button variant="contained" size="small" style={{background:'#157F1F'}}>Add Product <AddIcon style={{ fontSize:'1.2rem', marginBottom:'0.1rem', marginLeft:'0.1rem'}} /></Button>
+        <Button onClick={() => setCreateModal(true)} variant="contained" size="small" style={{background:'#157F1F'}}>Add Product <AddIcon style={{ fontSize:'1.2rem', marginBottom:'0.1rem', marginLeft:'0.1rem'}} /></Button>
       </div>
         <AdminControlStoreProductCard />
         <AdminControlStoreProductCard />
